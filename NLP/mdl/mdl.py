a7f24343c24a2b685bedc5bc111c4166225422fd
loser <3088655042@qq.com>
2025-07-30 17:32:35 +0800

    feat:bert

---
+++ NLP/mdl/mdl.py
@@ -0,0 +1,32 @@
+import torch
+from torch import nn
+from d2l import torch as d2l
+import collections
+
+def count_corpus(tokens):  #@save
+    """统计词元的频率"""
+    # 这里的tokens是1D列表或2D列表
+    if len(tokens) == 0 or isinstance(tokens[0], list):
+        # 将词元列表展平成一个列表
+        tokens = [token for line in tokens for token in line]
+    return collections.Counter(tokens)
+
+# Transformer Encoder Block
+#@save
+class EncoderBlock(nn.Module):
+    """Transformer编码器块"""
+    def __init__(self, key_size, query_size, value_size, num_hiddens,
+                 norm_shape, ffn_num_input, ffn_num_hiddens, num_heads,
+                 dropout, use_bias=False, **kwargs):
+        super(EncoderBlock, self).__init__(**kwargs)
+        self.attention = d2l.MultiHeadAttention(
+            num_hiddens, num_heads, dropout,
+            use_bias)
+        self.addnorm1 = d2l.AddNorm(norm_shape, dropout)
+        self.ffn = d2l.PositionWiseFFN(
+            ffn_num_hiddens, num_hiddens)
+        self.addnorm2 = d2l.AddNorm(norm_shape, dropout)
+
+    def forward(self, X, valid_lens):
+        Y = self.addnorm1(X, self.attention(X, X, X, valid_lens))
+        return self.addnorm2(Y, self.ffn(Y))
